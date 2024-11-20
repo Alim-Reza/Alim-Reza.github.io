1fdd7a8c876b428e3f5c44c7a15d0dcabbd02917
alim-reza <aarasif2@gmail.com>
2024-11-20 13:01:17 +0600

    slite changes

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-import './style.css'
-import AOS from 'aos'
-import 'aos/dist/aos.css'
+import './style.css';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 // Initialize AOS
-AOS.init()
+AOS.init();
 
 const experience = [
   {
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/', // Adjust if needed for subdirectories
   build: {
     outDir: 'dist', // Output directory for built files
     emptyOutDir: true, // Clear output directory before building
